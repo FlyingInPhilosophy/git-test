d3da26009f516c29931eefeeb6c360c8331b1036
Flying <3287511877@qq.com>
2023-05-20 15:29:57 +0800

    push commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,5 +11,6 @@ public class Main {
         System.out.println("hello git!");
         System.out.println("hello merge in master");
         System.out.println("hello merge in b1");
+        System.out.println("push into github");
     }
 }
