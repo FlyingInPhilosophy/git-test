79902e1d9e2c6b849385217c805723e6e01158d1
Flying <134047146+FlyingInPhilosophy@users.noreply.github.com>
2023-05-20 16:12:55 +0800

    pull commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,5 +13,6 @@ public class Main {
         System.out.println("hello merge in b1");
         System.out.println("push into github");
         System.out.println("push test");
+        sysetem.out.println("pull test");
     }
 }
