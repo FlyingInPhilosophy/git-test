2600cbcff10c5e71ed450e9f7ff929a1171f7aa5
Flying <3287511877@qq.com>
2023-05-20 14:35:41 +0800

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+
+/**
+ * @ author Flying
+ * @ create 2023/5/20 14:19
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello git!");
+    }
+}
